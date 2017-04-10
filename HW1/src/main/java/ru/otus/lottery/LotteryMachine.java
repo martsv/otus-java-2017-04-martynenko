b49585f72496ca2d494edd34611866f080bd3d90
martsv <martsv@bk.ru>
2017-04-10 22:23:41 +0300

    HW1

---
+++ HW1/src/main/java/ru/otus/lottery/LotteryMachine.java
@@ -0,0 +1,43 @@
+package ru.otus.lottery;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+/**
+ * Created by tully.
+ */
+class LotteryMachine {
+    private final List<String> emails;
+    private final int winnersCount;
+    private long seed = 0;
+
+    LotteryMachine(List<String> emails, int winnersCount) {
+        this.emails = emails;
+        this.winnersCount = Math.min(winnersCount, emails.size());
+    }
+
+    List<String> draw() {
+        System.out.println("Draw for the seed: " + seed);
+        Random rnd = new Random(seed);
+        Set<String> winners = new HashSet<>();
+        while (winners.size() < winnersCount) {
+            int index = rnd.nextInt(emails.size());
+            System.out.println("Ball: " + index);
+            winners.add(emails.get(index));
+        }
+        return new ArrayList<>(winners);
+    }
+
+    LotteryMachine setSeed(long seed) {
+        this.seed = seed;
+        return this;
+    }
+
+    LotteryMachine setSeed(String str) {
+        this.seed = str.hashCode();
+        return this;
+    }
+}
